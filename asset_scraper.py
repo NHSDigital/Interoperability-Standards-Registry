d6dafb621977481db86863df27175b50a0ec520a
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2024-10-09 09:24:57 +0100

    Update asset_scraper.py

--- asset_scraper.py
+++ asset_scraper.py
@@ -156,8 +156,8 @@ capabilitystatements = dict(sorted(capabilitystatements.items()))
 
 '''Create markdown file'''
 def code_assets(asset,elements):
-    print(f"<tr>\n  <td>{str(asset)}</td>\n", file = md_file)
-    print(f'''<td><a href="{elements['repo_name']}/{elements['id']}">{elements['id']}</a></td>\n''',file=md_file)
+    #print(f"<tr>\n  <td>{str(asset)}</td>\n", file = md_file)
+    print(f'''<td><a href="https://simplifier.net/{elements['repo_name']}/{elements['id']}">{elements['id']}</a></td>\n''',file=md_file)
     print(f"{elements['repo_name']}/{elements['id']}")
     for element,value in elements.items():
         print(f"  <td> {str(value)} </td>\n", file = md_file)
