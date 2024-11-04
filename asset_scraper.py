5e4b69c2d2c1912c8ce3d8f53d96a607fdde5a87
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2024-11-04 15:36:07 +0000

    Update asset_scraper.py

--- asset_scraper.py
+++ asset_scraper.py
@@ -113,46 +113,6 @@ def write_section(title, items):
         os.remove(page)
     md_file = open(page,"w")
 
-    print('''<label>
-    <input type="checkbox" id="ukcore-checkbox" checked>
-    Show UKCore Items
-    </label>
-    <br>
-    <label>
-    <input type="checkbox" id="nhsengland-checkbox" checked>
-    Show NHSEngland Items
-    </label>
-
-    ''',file=md_file)
-    print('''<script>
-    const ukcoreCheckbox = document.getElementById('ukcore-checkbox');
-    const nhsenglandCheckbox = document.getElementById('nhsengland-checkbox');
-
-    ukcoreCheckbox.addEventListener('change', function() {
-        const ukcoreItems = document.querySelectorAll('.ukcore');
-        ukcoreItems.forEach(item => {
-        if (ukcoreCheckbox.checked) {
-            item.classList.remove('hidden');
-        } else {
-            item.classList.add('hidden');
-        }
-        });
-    });
-
-    nhsenglandCheckbox.addEventListener('change', function() {
-        const nhsenglandItems = document.querySelectorAll('.nhsengland');
-        nhsenglandItems.forEach(item => {
-        if (nhsenglandCheckbox.checked) {
-            item.classList.remove('hidden');
-        } else {
-            item.classList.add('hidden');
-        }
-        });
-    });
-    </script>
-
-    ''',file=md_file)
-
     print(f'''## {title}s\n\n''', file=md_file)
     profile_header = ''
     for asset, elements in items.items():
